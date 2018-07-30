899e170cf272b8c751474c38eba23a4ad9f79caf
mkleiner <kleiner.matias@gmail.com>
2018-07-30 14:10:09 -0300

    lectura de datos de estado ok

--- examples_modules/cli_mxp/cli-mxp.c
+++ examples_modules/cli_mxp/cli-mxp.c
@@ -46,7 +46,8 @@
 #include <sys/mman.h>
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>
-
+#include<stdio.h>
+#include<math.h>
 
 /* module static variables */
 static ncx_module_t *cli_mxp_mod;
@@ -61,6 +62,67 @@ Monitor *pt_monitor_struct;
 int shmfd;
 
 
+
+// reverses a string 'str' of length 'len'
+void reverse(char *str, int len)
+{
+    int i=0, j=len-1, temp;
+    while (i<j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++; j--;
+    }
+}
+
+ // Converts a given integer x to string str[].  d is the number
+ // of digits required in output. If d is more than the number
+ // of digits in x, then 0s are added at the beginning.
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+    while (x)
+    {
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+ 
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+ 
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
+ 
+// Converts a floating point number to string.
+void ftoa(float n, char *res, int afterpoint)
+{
+    // Extract integer part
+    int ipart = (int)n;
+ 
+    // Extract floating part
+    float fpart = n - (float)ipart;
+ 
+    // convert integer part to string
+    int i = intToStr(ipart, res, 0);
+ 
+    // check for display option after point
+    if (afterpoint != 0)
+    {
+        res[i] = '.';  // add dot
+ 
+        // Get the value of fraction part upto given no.
+        // of points after dot. The third parameter is needed
+        // to handle cases like 233.007
+        fpart = fpart * pow(10, afterpoint);
+ 
+        intToStr((int)fpart, res + i + 1, afterpoint);
+    }
+}
 
 /********************************************************************
 * FUNCTION y_cli_mxp_init_static_vars
@@ -742,13 +804,13 @@ static status_t cli_mxp_mux_state_fpga_temperature_get (
 
   
 
+  char buff[20];
 
   float fpga_temperatur=pt_monitor_struct->general_struct.fpga_temperature;
-
-
+  ftoa(fpga_temperatur, buff, 2);
 
   /* set the fpga_temperature var here, change zero */
-  fpga_temperatures = (const xmlChar *)"0.1";
+  fpga_temperatures = (const xmlChar *)buff;
   res = val_set_simval_obj(
     dstval,
     dstval->obj,
@@ -1310,4 +1372,3 @@ void y_cli_mxp_cleanup (void)
 } /* y_cli_mxp_cleanup */
 
 /* END cli_mxp.c */
-
