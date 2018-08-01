f04b62e978aca67b5dd440c53b5fb4fd7be47818
Matias <kleiner.matias@gmail.com>
2018-08-01 15:31:04 -0300

    modulo remoto

--- examples_modules/utils_scripts/pruba.sh
+++ examples_modules/utils_scripts/pruba.sh
@@ -18,9 +18,9 @@ read arch
 libmodule=lib$module
 
 if [ "$arch" == "nios2" ]; then
-	echo "You have access!"
+	gcc -shared  -fPIC -DPIC  -I. -I../../compile_yuma123/$arch/output/usrapp/include/yuma/agt -I../../compile_yuma123/$arch/output/usrapp/include/yuma/ncx -I../../compile_yuma123/$arch/output/usrapp/include/yuma/platform -I../../compile_yuma123/$arch/output/usrapp/include/libxml2 -I../../compile_yuma123/$arch/output/usrapp/include/libxml2/libxml -rdynamic ../$module/$module.c ../../compile_yuma123/$arch/output/usrapp/lib/libyumancx.so ../../compile_yuma123/$arch/output/usrapp/lib/libyumaagt.so ../../compile_yuma123/$arch/output/usrapp/lib/libxml2.so -lz -ldl -O0 -o ../$module/$libmodule.so
 elif [ "$arch" == "arm" ]; then
-	echo "ACCESS DENIED!"
+	gcc -shared  -fPIC -DPIC  -I. -I../../compile_yuma123/$arch/output/usrapp/include/yuma/agt -I../../compile_yuma123/$arch/output/usrapp/include/yuma/ncx -I../../compile_yuma123/$arch/output/usrapp/include/yuma/platform -I../../compile_yuma123/$arch/output/usrapp/include/libxml2 -I../../compile_yuma123/$arch/output/usrapp/include/libxml2/libxml -rdynamic ../$module/$module.c ../../compile_yuma123/$arch/output/usrapp/lib/libyumancx.so ../../compile_yuma123/$arch/output/usrapp/lib/libyumaagt.so ../../compile_yuma123/$arch/output/usrapp/lib/libxml2.so -lz -ldl -O0 -o ../$module/$libmodule.so
 elif [ "$arch" == "x86_64" ]; then
 	gcc -shared  -fPIC -DPIC  -I. -I../../compile_yuma123/$arch/output/usrapp/include/yuma/agt -I../../compile_yuma123/$arch/output/usrapp/include/yuma/ncx -I../../compile_yuma123/$arch/output/usrapp/include/yuma/platform -I../../compile_yuma123/$arch/output/usrapp/include/libxml2 -I../../compile_yuma123/$arch/output/usrapp/include/libxml2/libxml -rdynamic ../$module/$module.c ../../compile_yuma123/$arch/output/usrapp/lib/libyumancx.so ../../compile_yuma123/$arch/output/usrapp/lib/libyumaagt.so ../../compile_yuma123/$arch/output/usrapp/lib/libxml2.so -lz -ldl -O0 -o ../$module/$libmodule.so
 fi
