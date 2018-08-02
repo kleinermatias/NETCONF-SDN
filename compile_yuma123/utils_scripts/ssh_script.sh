cf0a066705280ae65165646bf8c707562009ce15
Ragnar Lodbrok <kleiner.matias@gmail.com>
2018-08-02 13:28:27 -0300

    Update ssh_script.sh

--- compile_yuma123/utils_scripts/ssh_script.sh
+++ compile_yuma123/utils_scripts/ssh_script.sh
@@ -3,16 +3,20 @@
 
 echo "------- Copy sshd_config -------"
 
+#Copio el archivo sshd_config que se encuentra en la carpeta NETCONF-SDN/compile_yuma123/utils_scripts/
 cp -r -f /root/usrapp/sshd_config /etc/ssh/
 
 echo "------- Restart ssh server -------"
 
+#Reinicio el servicio ssh
 /etc/init.d/S50sshd restart
 
+#Ejecuto los scripts
 sh /root/init_bridge_tx.sh
 sh /root/init_bridge_rx.sh
 
 echo "------- EXPORT PATH -------"
+
 
 echo "export PATH=$PATH:/root/usrapp/sbin" >> ~/.bashrc
 . ~/.bashrc
