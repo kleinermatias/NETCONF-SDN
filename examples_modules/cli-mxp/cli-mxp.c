ef59442ed9575c76dc036f47da282989a44aabb5
Matias <kleiner.matias@gmail.com>
2018-08-02 23:41:53 -0300

    prueba2

--- examples_modules/cli-mxp/cli-mxp.c
+++ examples_modules/cli-mxp/cli-mxp.c
@@ -956,6 +956,8 @@ static status_t cli_mxp_mux_state_board_humidity_state_get (
   /* set the board_humidity_state var here, change zero */
   /* set the fpga_temperature var here, change zero */
 
+  printf("%s\n", "LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG");
+  printf("%s\n", (const xmlChar *)pt_monitor_struct->general_struct.board_humidity);
   //(const xmlChar *)pt_monitor_struct->general_struct.board_humidity;
   board_humidity_state = (const xmlChar *)pt_monitor_struct->general_struct.board_humidity;
   res = val_set_simval_obj(
