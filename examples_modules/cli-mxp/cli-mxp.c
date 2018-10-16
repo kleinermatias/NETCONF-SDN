6a4bf57ae271a8175637193fde36283e2b5b8285
matias <kleiner.matias@gmail.com>
2018-10-16 10:06:31 -0300

    agrego todas las variables de estado MISC

--- examples_modules/cli-mxp/cli-mxp.c
+++ examples_modules/cli-mxp/cli-mxp.c
@@ -2032,7 +2032,7 @@ static status_t cli_mxp_mux_state_misc_tx_laser_itu_band_get (
   }
 
   /* set the tx_laser_itu_band var here, change EMPTY_STRING */
-  float tx_laser_itu_band_float=pt_monitor_struct->txp_struct->txp_tx_laser_itu_channel.fields.band;
+  float tx_laser_itu_band_float=pt_monitor_struct->txp_struct.txp_tx_laser_itu_channel.fields.band;
   char buf[30];
   sprintf(buf, "%c C", tx_laser_itu_band_float);
   tx_laser_itu_band = (const xmlChar *)buf;
