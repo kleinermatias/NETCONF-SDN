608fb440ce47865257d8d673801a8c7db61219d5
ragnar-l <kleiner.matias@gmail.com>
2019-03-11 21:36:33 -0300

    update python app web

--- python-app/mypy.py
+++ python-app/mypy.py
@@ -32,7 +32,7 @@ bootstrap = Bootstrap(app)
 def dispositivos():
     global devices
     devices = [] #armo lista de dispositivos
-    response = requests.get('http://172.16.0.221:8181/onos/v1/devices', headers=headers, auth=('karaf', 'karaf')).text
+    response = requests.get('http://localhost:8181/onos/v1/devices', headers=headers, auth=('karaf', 'karaf')).text
     data = json.loads(response) #paso respuesta del curl a json
     cantidad_dispositivos = len(data["devices"]) #obtengo la cantidad de los dispositivos
     for x in range(0, cantidad_dispositivos):
@@ -44,7 +44,7 @@ def alarms():
     global params
     global alarmas
     global cantidad_alarmas
-    alarmas = requests.get('http://172.16.0.221:8181/onos/v1/fm/alarms', headers=headers, params=params, auth=('karaf', 'karaf')).text
+    alarmas = requests.get('http://localhost:8181/onos/v1/fm/alarms', headers=headers, params=params, auth=('karaf', 'karaf')).text
     data_alarm = json.loads(alarmas) #paso respuesta del curl a json
     cantidad_alarmas = len(data_alarm["alarms"]) #obtengo la cantidad de los dispositivos
     alarmas = []
@@ -63,7 +63,7 @@ def config_all(bool,name):
             params = (
                 ('devId', str(x)),
             )
-            config = requests.get('http://172.16.0.221:8181/onos/altura/GET/Config%20Data/'+str(x), headers=headers, params=params, auth=('karaf', 'karaf')).text
+            config = requests.get('http://localhost:8181/onos/altura/GET/Config%20Data/'+str(x), headers=headers, params=params, auth=('karaf', 'karaf')).text
             tipo_trafico = "TIPO DE TRAFICO: "+config[(config.index("<tipo_trafico>")+len("<tipo_trafico>")):config.index("</tipo_trafico>")]
             tipo_fec_linea = "TIPO FEC LINEA: "+config[(config.index("<tipo_fec_linea>")+len("<tipo_fec_linea>")):config.index("</tipo_fec_linea>")]
             tipo_fec_cliente = "TIPO FEC CLIENTE: "+config[(config.index("<tipo_fec_cliente>")+len("<tipo_fec_cliente>")):config.index("</tipo_fec_cliente>")]
@@ -76,7 +76,7 @@ def config_all(bool,name):
         params = (
                 ('devId', name),
             )
-        config = requests.get('http://172.16.0.221:8181/onos/altura/GET/Config%20Data/'+name, headers=headers, params=params, auth=('karaf', 'karaf')).text
+        config = requests.get('http://localhost:8181/onos/altura/GET/Config%20Data/'+name, headers=headers, params=params, auth=('karaf', 'karaf')).text
         tipo_trafico = "TIPO DE TRAFICO: "+config[(config.index("<tipo_trafico>")+len("<tipo_trafico>")):config.index("</tipo_trafico>")]
         tipo_fec_linea = "TIPO FEC LINEA: "+config[(config.index("<tipo_fec_linea>")+len("<tipo_fec_linea>")):config.index("</tipo_fec_linea>")]
         tipo_fec_cliente = "TIPO FEC CLIENTE: "+config[(config.index("<tipo_fec_cliente>")+len("<tipo_fec_cliente>")):config.index("</tipo_fec_cliente>")]
@@ -91,28 +91,28 @@ def config_tipo_trafico(tipo,device):
     global alarmas
     global devices
     global matches
-    config = requests.put('http://172.16.0.221:8181/onos/altura/SET/Tipo%20de%20Trafico/'+str(device)+','+str(tipo), headers=headers, auth=('karaf', 'karaf')).text
+    config = requests.put('http://localhost:8181/onos/altura/SET/Tipo%20de%20Trafico/'+str(device)+','+str(tipo), headers=headers, auth=('karaf', 'karaf')).text
 
 def config_tipo_linea(tipo,device):
     global params
     global alarmas
     global devices
     global matches
-    config = requests.put('http://172.16.0.221:8181/onos/altura/SET/Tipo%20Fec%20de%20linea/'+str(device)+','+str(tipo), headers=headers, auth=('karaf', 'karaf')).text
+    config = requests.put('http://localhost:8181/onos/altura/SET/Tipo%20Fec%20de%20linea/'+str(device)+','+str(tipo), headers=headers, auth=('karaf', 'karaf')).text
 
 def config_tipo_cliente(tipo,device):
     global params
     global alarmas
     global devices
     global matches
-    config = requests.put('http://172.16.0.221:8181/onos/altura/SET/Tipo%20Fec%20de%20Cliente/'+str(device)+','+str(tipo), headers=headers, auth=('karaf', 'karaf')).text
+    config = requests.put('http://localhost:8181/onos/altura/SET/Tipo%20Fec%20de%20Cliente/'+str(device)+','+str(tipo), headers=headers, auth=('karaf', 'karaf')).text
 
 def rpc_apply_config(device):
     global params
     global alarmas
     global devices
     global matches
-    config = requests.put('http://172.16.0.221:8181/onos/altura/RPC/Apply%20Config/'+str(device), headers=headers, auth=('karaf', 'karaf')).text
+    config = requests.put('http://localhost:8181/onos/altura/RPC/Apply%20Config/'+str(device), headers=headers, auth=('karaf', 'karaf')).text
 
 def pareja_dispositivos(device):
     global params
@@ -120,7 +120,7 @@ def pareja_dispositivos(device):
     global devices
     global matches
     global parejas
-    config = requests.get('http://172.16.0.221:8181/onos/altura/GET/Config%20Data/'+str(device), headers=headers, auth=('karaf', 'karaf')).text
+    config = requests.get('http://localhost:8181/onos/altura/GET/Config%20Data/'+str(device), headers=headers, auth=('karaf', 'karaf')).text
 
 def estado_link_logico():
     global params
@@ -129,7 +129,7 @@ def estado_link_logico():
     global matches
     global parejas
     global cantidad_link_logico
-    config = requests.get('http://172.16.0.221:8181/onos/v1/links', headers=headers, auth=('karaf', 'karaf')).text
+    config = requests.get('http://localhost:8181/onos/v1/links', headers=headers, auth=('karaf', 'karaf')).text
     data_link = json.loads(config) #paso respuesta del curl a json
     cantidad_links = len(data_link["links"]) #obtengo la cantidad de los dispositivos
     links = []
@@ -145,7 +145,7 @@ def uri_to_serial_number(device):
     global devices
     serial_number = ""
     devices = [] #armo lista de dispositivos
-    response = requests.get('http://172.16.0.221:8181/onos/v1/devices', headers=headers, auth=('karaf', 'karaf')).text
+    response = requests.get('http://localhost:8181/onos/v1/devices', headers=headers, auth=('karaf', 'karaf')).text
     data = json.loads(response) #paso respuesta del curl a json
     cantidad_dispositivos = len(data["devices"]) #obtengo la cantidad de los dispositivos
     for x in range(0, cantidad_dispositivos):
@@ -308,7 +308,7 @@ def boton_agregar_dispositivo():
         f.write(json_data)
         f.close()
         data = open('holamundo.json')
-        response = requests.post('http://172.16.0.221:8181/onos/v1/network/configuration', headers=headers, data=data, auth=('onos', 'rocks'))
+        response = requests.post('http://localhost:8181/onos/v1/network/configuration', headers=headers, data=data, auth=('onos', 'rocks'))
     return render_template('index.html', devices=devices, id_devices_html=devices, alarmas=alarmas , configuracion=matches , var1=cantidad_alarmas, linklogico=cantidad_link_logico,
     perfiles=nombre_perfil)
 
